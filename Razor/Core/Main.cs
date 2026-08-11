fc900ba3fc6a2a37d02512ea17711dddd07b1c8f
angel6929 <duwei6929@gmail.com>
2026-08-11 18:12:11 +0800

    更新窗口标题日期至 20260811

--- Razor/Core/Main.cs
+++ Razor/Core/Main.cs
@@ -162,7 +162,7 @@ namespace Assistant
             {
                 if (m_Version == null)
                 {
-                    m_Version = "20260804（阳光大陆专用版）";
+                    m_Version = "20260811（阳光大陆专用版）";
                 }
 
                 return m_Version;
